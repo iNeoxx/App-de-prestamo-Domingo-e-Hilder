dad2f0ce415a41e4f44e2b23c010b619bef18e7e
iNeoxx <gmdneoxx1@gmail.com>
2023-06-04 16:14:01 -0600

    App de prestamo

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+        ¿Cuánto <span class="text-indigo-600">dinero</span> Necesitas?
+        </h1>
+        );
+        
+};
+export default Header;
